332f2a669a59ebf504d7942017eeaaa7349b98ca
WongWeiFong <weifong041623@gmail.com>
2026-02-03 18:43:26 +0800

    added vercel logic

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,17 +10,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    // origin: process.env.FRONTEND_URL,
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // credentials: true,
   });
 
-  const prismaService = app.get(PrismaService);
-  const users = await prismaService.user.findMany();
-  console.log('Users in database:', users);
-  console.log('Total users:', users.length);
+  await app.init();
+  return app.getHttpAdapter().getInstance();
+
+  // const prismaService = app.get(PrismaService);
+  // const users = await prismaService.user.findMany();
+  // console.log('Users in database:', users);
+  // console.log('Total users:', users.length);
 
   await app.listen(process.env.PORT ?? 3000);
-  console.log("abc", process.env.GOOGLE_APPLICATION_CREDENTIALS);
+  // console.log("abc", process.env.GOOGLE_APPLICATION_CREDENTIALS);
 }
-bootstrap();
+export default bootstrap();
