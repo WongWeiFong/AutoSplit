5a35c490b0c267231db1d6a9b7f48e528ad14462
WongWeiFong <weifong041623@gmail.com>
2026-02-04 12:59:21 +0800

    make api to serverless

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,30 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import { PrismaService } from './prisma/prisma.service';
-import 'dotenv/config';
 
-dotenv.config();
+let server: any;
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+export default async function handler(req: any, res: any) {
+  if (!server) {
+    const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: process.env.VITE_FRONTEND_URL,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
-  });
+    app.enableCors({
+      origin: process.env.FRONTEND_URL,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+      allowedHeaders: 'Content-Type, Accept, Authorization',
+    });
 
-  await app.init();
-  return app.getHttpAdapter().getInstance();
+    await app.init();
+    server = app.getHttpAdapter().getInstance();
+  }
 
-  // const prismaService = app.get(PrismaService);
-  // const users = await prismaService.user.findMany();
-  // console.log('Users in database:', users);
-  // console.log('Total users:', users.length);
-
-  await app.listen(process.env.PORT ?? 3000);
-  // console.log("abc", process.env.GOOGLE_APPLICATION_CREDENTIALS);
+  return server(req, res);
 }
-export default bootstrap();
